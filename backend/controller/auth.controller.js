976da8e6448a59b44b7064bcb9b026fcefe35b1a
waseemzahid <waseemzahid64@gmail.com>
2024-05-15 16:59:23 +0500

    backend completed

---
+++ backend/controller/auth.controller.js
@@ -0,0 +1,89 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import generateTokenAndSetCookies from "../utils/generateToken.js";
+
+export const signup = async(req, res) => {
+    try {
+        const{fullName, username, password, confirmPassword, gender} = req.body;
+
+        if (password !== confirmPassword) {
+			return res.status(400).json({ error: "Passwords don't match" });
+		}
+
+		const user = await User.findOne({ username });
+
+		if (user) {
+			return res.status(400).json({ error: "Username already exists" });
+		}
+
+		// HASH PASSWORD HERE
+		const salt = await bcrypt.genSalt(10);
+		const hashedPassword = await bcrypt.hash(password, salt);
+
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
+
+        const newUser = new User({
+            fullName,
+            username,
+            password:hashedPassword, 
+            gender,
+            profilePic: gender === 'male' ? boyProfilePic : girlProfilePic 
+        })
+
+        if (newUser){
+            generateTokenAndSetCookies(newUser._id, res);
+            await newUser.save()
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                username: newUser.username,
+                gender: newUser.gender,
+                profilePic: newUser.profilePic
+            })
+        }
+        else{
+            res.status(400).json({ error: "Invalid User Data" });
+        }
+    } catch (error) {
+        console.log("Error in signup controller", error.message);
+        return res.status(500).json({ error: "Intervel Server Error" });
+    }
+}
+
+export const login = async(req, res) => {
+    try {
+        const{username, password} = req.body;
+        const user = await User.findOne({username})
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || "")
+
+        if(!user || !isPasswordCorrect){
+            return res.status(400).json({ error: "Invalid username or password" });
+        }
+
+            generateTokenAndSetCookies(user._id, res);
+
+            res.status(201).json({
+                _id: user._id,
+                fullName: user.fullName,
+                username: user.username,
+                gender: user.gender,
+                profilePic: user.profilePic
+            })
+
+    } catch (error) {
+        console.log("Error in signup controller", error.message);
+        return res.status(500).json({ error: "Intervel Server Error" }); 
+    }
+}
+
+export const logout = (req, res) => {
+    try{
+        res.cookie("jwt", "", { maxAge: 0 })
+        res.status(200).json({ message: "Logout Successfully" });
+    } catch (error) {
+        console.log("Error in logout controller", error.message);
+        return res.status(500).json({ error: "Intervel Server Error" }); 
+    }
+}
